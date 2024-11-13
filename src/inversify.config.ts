33161becd3541dee0e1d92171c39a9b18a567f65
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-11-13 23:17:05 +0100

    fix: bind event bus service to contstant value

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,7 +4,10 @@ import { Container } from 'inversify';
 
 import { EventBusService } from './services/event-bus.service';
 import { TYPES } from './types';
+import { IEventBusService } from './types/interfaces/event-bus.service';
 
 export const container = new Container();
 
-container.bind<EventBusService>(TYPES.EventBusService).to(EventBusService);
+container
+  .bind<IEventBusService>(TYPES.EventBusService)
+  .toConstantValue(new EventBusService());
