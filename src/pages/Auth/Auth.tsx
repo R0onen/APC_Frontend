0d51c7f57c16916fad404c7e964bad42a85e9d29
R0onen <erni100105@gmail.com>
2024-11-14 15:33:35 +0500

    auth page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,10 +1,38 @@
+import { Button } from '../../components/Button';
+import styles from './Auth.module.css';
+
 function Auth() {
   return (
-    <div>
-      <div>
-        <h1>Логин</h1>
+    <body className={styles.bodyContainer}>
+      <div className={styles.loginContainer}>
+        <h1 className={styles.heading}>Логин</h1>
+        <form>
+          <div className={styles.formField}>
+            <label htmlFor="email" className={styles.label}>
+              Email
+            </label>
+            <input type="email" id="email" required className={styles.input} />
+          </div>
+          <div className={styles.formField}>
+            <label htmlFor="password" className={styles.label}>
+              Пароль
+            </label>
+            <input
+              type="password"
+              id="password"
+              required
+              className={styles.input}
+            />
+          </div>
+          <Button title="Войти" onClick={() => {}}></Button>
+        </form>
+        <div className={styles.footer}>
+          <button>Забыли пароль?</button>
+          <div className={styles.verticalLine}></div>
+          <button>Регистрация</button>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
